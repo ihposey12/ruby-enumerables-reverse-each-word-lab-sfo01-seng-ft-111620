deae8b662e104c3833a7cbc2ccaf8aa910c251cb
static-tutorial-1700 <ihposey@crimson.ua.edu>
2020-10-31 05:54:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(strings)
-  new_string = strings.split("")
+  new_string = strings.split(" ")
   array = []
   new_string.each do |strang|
     array << strang.reverse
   end
-  array.join("")
+  array.join(" ")
 end
